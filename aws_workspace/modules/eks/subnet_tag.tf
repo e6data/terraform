dcba14fabaaef87d529f8b0cc7e3a8f85274b489
Srinath Prabhu <srinath@e6x.io>
2023-08-18 14:44:25 +0530

    fixes

--- aws_workspace/modules/eks/subnet_tag.tf
+++ aws_workspace/modules/eks/subnet_tag.tf
@@ -1,6 +1,6 @@
 resource "aws_ec2_tag" "subnet_cluster_tag" {
   count    =    length(var.subnet_ids)
   resource_id = var.subnet_ids[count.index]
-  key         = "kubernetes.io/cluster/${var.cluster_name}"
+  key         = "kubernetes.io/cluster/e6data-${var.cluster_name}"
   value       = "shared"
 }
