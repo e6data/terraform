df0eed67fbb30a05f7846d9eca9e63d2d9b3324c
pranav-e6x <pranav@e6x.io>
2023-08-20 00:31:24 +0530

    updated output values

--- aws/output.tf
+++ aws/output.tf
@@ -1,17 +1,13 @@
-output "e6data_workspace_name" {
+output "workspace_name" {
   value = var.workspace_name
-}
-
-output "workspace_s3_bucket_name" {
-  value = module.e6data_s3_bucket.name
 }
 
 output "region" {
   value = var.aws_region
 }
 
-output "eks_nodegroup_name" {
-  value = aws_eks_node_group.workspace_node_group.node_group_name
+output "workspace_s3_bucket_name" {
+  value = module.e6data_s3_bucket.name
 }
 
 output "cross_account_role_arn" {
@@ -19,13 +15,17 @@ output "cross_account_role_arn" {
 }
 
 output "kubernetes_cluster_name" {
-  value = var.eks_cluster_name
+  value = module.eks.cluster_name
 }
 
-output "kubernetes_namespace_for_e6data_workspace" {
-  value = var.kubernetes_namespace
+output "eks_nodegroup_name" {
+  value = aws_eks_node_group.workspace_node_group.node_group_name
 }
 
 output "eks_nodegroup_max_instances" {
   value = var.max_instances_in_eks_nodegroup
+}
+
+output "kubernetes_namespace" {
+  value = var.kubernetes_namespace
 }
