82b1de9188f1528dee7d69621bc6af62f7f60f2b
pranav-e6x <129151401+pranav-e6x@users.noreply.github.com>
2023-08-22 15:37:31 +0530

    updated output file (#9)
    * updated output file
    
    * updated output values

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,3 +1,23 @@
+output "workspace_name" {
+  value = var.workspace_name
+}
+
+output "region" {
+  value = var.gcp_region
+}
+
+output "workspace_gcs_bucket_name" {
+  value = local.e6data_workspace_name
+}
+
+output "gcp_project_id" {
+  value = var.gcp_project_id
+}
+
+output "kubernetes_cluster_name" {
+  value = var.cluster_name
+}
+
 output "gke_nodepool_name" {
   value = local.e6data_workspace_name
 }
@@ -6,30 +26,10 @@ output "gke_nodepool_max_instances" {
   value = var.max_instances_in_nodegroup
 }
 
-output "region" {
-  value = var.gcp_region
+output "kubernetes_namespace" {
+  value = var.kubernetes_namespace
 }
 
 output "kubernetes_cluster_zone" {
   value = var.kubernetes_cluster_zone
-}
-
-output "workspace_gcs_bucket_name" {
-  value = local.e6data_workspace_name
-}
-
-output "e6data_workspace_name" {
-  value = var.workspace_name
-}
-
-output "kubernetes_cluster_name" {
-  value = var.cluster_name
-}
-
-output "kubernetes_namespace_for_e6data_workspace" {
-  value = var.kubernetes_namespace
-}
-
-output "gcp_project_id" {
-  value = var.gcp_project_id
 }
