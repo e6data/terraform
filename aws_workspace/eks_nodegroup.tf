dedd2761d2012e38539d1dac3a24d298446bacf2
Srinath Prabhu <srinath@e6x.io>
2023-08-25 18:13:43 +0530

    <Sri>Nodegroups tags fix

--- aws_workspace/eks_nodegroup.tf
+++ aws_workspace/eks_nodegroup.tf
@@ -73,7 +73,7 @@ resource "aws_eks_node_group" "workspace_node_group" {
   tags = merge({
     "Name" = local.e6data_workspace_name
     "k8s.io/cluster-autoscaler/enabled" =  "true"
-    "k8s.io/cluster-autoscaler/${var.eks_cluster_name}" = "owned"
+    "k8s.io/cluster-autoscaler/${module.eks.cluster_name}" = "owned"
   }, var.cost_tags )
 
   lifecycle {
