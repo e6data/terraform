bedf57e419f069697e78ce120129cad609834d5d
Srinath Prabhu <srinath@e6x.io>
2023-08-30 12:30:16 +0530

    <Sri>fixes on minimum nodes of default node group

--- aws_workspace/default_nodegroup.tf
+++ aws_workspace/default_nodegroup.tf
@@ -55,9 +55,9 @@ resource "aws_eks_node_group" "default_node_group" {
   }
 
   scaling_config {
-    min_size     = 1
-    desired_size = 1
-    max_size     = 2
+    min_size     = 2
+    desired_size = 2
+    max_size     = 3
   }
 
   update_config {
