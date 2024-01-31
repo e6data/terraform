d6a85ff26ee27a60cac532cde00cf79fb6f39a6e
Srinath Prabhu <srinath@e6x.io>
2024-01-31 11:42:02 +0530

    <Sri>Adding alb ingress controller permissions

--- e6data_with_existing_eks/cross_account_iam.tf
+++ e6data_with_existing_eks/cross_account_iam.tf
@@ -48,6 +48,206 @@ data "aws_iam_policy_document" "cross_account_iam_eksAccess_doc" {
       "arn:aws:eks:${var.aws_region}:${data.aws_caller_identity.current.account_id}:nodegroup/${var.eks_cluster_name}/*/*"
     ]
   }
+
+  statement {
+    sid = "DescribeEC2ActionsandWAF2" 
+    effect = "Allow"
+    
+    actions = [
+      "ec2:CreateSecurityGroup",
+      "ec2:DescribeRouteTables",
+      "wafv2:GetWebACL",
+      "wafv2:DisassociateWebACL",
+      "wafv2:GetWebACLForResource",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeInstances",
+      "servicequotas:GetServiceQuota",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:SetWebACL"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "CreateTagsForSecurityGroup"
+    effect = "Allow"
+    actions = ["ec2:CreateTags"]
+    resources = ["arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:security-group/*"]
+    condition {
+      test = "StringEquals"
+      variable = "ec2:CreateAction"
+      values = ["CreateSecurityGroup"]
+    }
+
+    condition {
+      test = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = ["false"]
+    }
+  }
+
+  statement {
+    sid = "ManageSecurityGroup"
+    effect = "Allow"
+    actions = [
+      "ec2:CreateTags",
+      "ec2:DeleteTags"
+    ]
+    resources = ["arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:security-group/*"]
+
+    condition {
+      test = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = ["true"]
+    }
+
+    condition {
+      test = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values = ["true"]
+    }
+  }
+
+  statement {
+    sid = "DeleteSecurityGroup"
+    effect = "Allow"
+    actions = ["ec2:DeleteSecurityGroup"]
+    resources = ["*"]
+
+    condition {
+      test = "StringEquals"
+      variable = "ec2:ResourceTag/elbv2.k8s.aws/cluster"
+      values = ["false"]
+    }
+  }
+
+  statement {
+    sid = "AllowCreateELB"
+    effect = "Allow"
+    actions = ["elasticloadbalancing:CreateLoadBalancer", "elasticloadbalancing:DeleteLoadBalancer"]
+    resources = ["*"]
+
+    condition {
+      test = "Null"
+      variable = "ec2:ResourceTag/elbv2.k8s.aws/cluster"
+      values = ["false"]
+    }
+  }
+
+  statement {
+    sid = "ManageELBTags"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags"
+    ]
+
+    resources = [
+      "arn:aws:elasticloadbalancing:*:*:listener/net/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener/app/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*"
+    ]
+  }
+
+  statement {
+    sid    = "manageELBTargetGroupTags"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*",
+      "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+    ]
+
+    actions = [
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags",
+    ]
+
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values   = ["true"]
+    }
+
+    condition {
+      test     = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values   = ["false"]
+    }
+  }
+
+  statement {
+    sid    = "ALBIngressControllerTags"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*",
+    ]
+
+    actions = ["elasticloadbalancing:AddTags"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "elasticloadbalancing:CreateAction"
+
+      values = [
+        "CreateTargetGroup",
+        "CreateLoadBalancer",
+      ]
+    }
+
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values   = ["false"]
+    }
+  }
+
+  statement {
+    sid       = "AllowWAFv2WebACLAndIPSetCreation"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "wafv2:CreateWebACL",
+      "wafv2:CreateIPSet",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:RequestTag/app"
+      values   = ["e6data"]
+    }
+  }
+
+  statement {
+    sid       = "AllowWAFv2WebACLManagement"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "wafv2:TagResource",
+      "wafv2:AssociateWebACL",
+      "wafv2:DeleteWebACL",
+      "wafv2:UpdateWebACL",
+      "wafv2:GetIPSet",
+      "wafv2:CreateIPSet",
+      "wafv2:DeleteIPSet",
+      "wafv2:UpdateIPSet",
+      "wafv2:UntagResource",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/app"
+      values   = ["e6data"]
+    }
+  }
 }
 
 resource "aws_iam_policy" "e6data_s3_read_write_policy" {
