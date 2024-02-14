ab62898d3db092c4079af1b18536db3dcf4d3b77
Srinath Prabhu <srinath@e6x.io>
2024-02-14 12:56:42 +0530

    <Sri>fixes on nodegroup naming to use name_prefix

--- e6data_with_new_eks/support.tf
+++ e6data_with_new_eks/support.tf
@@ -1,5 +1,7 @@
 locals {
-  e6data_workspace_name = "e6data-workspace-${var.workspace_name}"
+  short_workspace_name = substr(var.workspace_name, 0, 5)
+
+  e6data_workspace_name = "e6data-workspace-${local.short_workspace_name}"
   bucket_names_with_full_path = [for bucket_name in var.bucket_names : "arn:aws:s3:::${bucket_name}/*"]
   bucket_names_with_arn = [for bucket_name in var.bucket_names : "arn:aws:s3:::${bucket_name}"]
 
