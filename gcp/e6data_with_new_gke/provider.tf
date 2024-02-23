c806167a795a74e01951494a55fa77b619d2b4e2
Harshith <harshith@e6x.io>
2024-02-23 11:18:05 +0530

    <harshith>connectivity v2

--- gcp/e6data_with_new_gke/provider.tf
+++ gcp/e6data_with_new_gke/provider.tf
@@ -1,19 +1,30 @@
 terraform {
+  backend "s3" {
+    bucket = "internal-terraform-state"
+    key    = "gcp/state.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.72.0"
+      version = "5.16.0"
     }
   }
 }
 
 # Comment out this block if storing Terraform state is required.
-
 provider "google" {
-    project = var.gcp_project_id
-    region = var.gcp_region
-    /* credentials = "{{GOOGLE_CLOUD_KEYFILE_JSON}}" */
-    # access_token = "{{ gcp_access_token }}"
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  credentials = "/home/ec2-user/e6-infra-internal/terraform-gcp/values/gcp_sa.json"
+  default_labels = var.cost_labels
+}
+
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  credentials = "/home/ec2-user/e6-infra-internal/terraform-gcp/values/gcp_sa.json"
+  default_labels = var.cost_labels
 }
 
 # Uncomment this block if storing Terraform state is required & update the bucket name.
