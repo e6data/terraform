e4e1e5aa75db1525a3040db673628d4edea88d1e
Srinath Prabhu <72329057+srinath-prabhu@users.noreply.github.com>
2024-03-13 11:18:42 +0530

    <Sri>fixes on nodegroup naming to use name_prefix (#45)
    * <Sri>fixes on nodegroup naming to use name_prefix

--- e6data_with_existing_eks/eks_nodegroup.tf
+++ e6data_with_existing_eks/eks_nodegroup.tf
@@ -1,6 +1,7 @@
 # Create EKS node group for workspace
 resource "aws_launch_template" "nodegroup_launch_template" {
-  name = "${local.e6data_workspace_name}-nodegroup-launch-template-${element(split(".", var.kube_version),1)}"
+  # name = "${local.e6data_workspace_name}-nodegroup-launch-template-${element(split(".", var.kube_version),1)}"
+  name_prefix = "${local.e6data_workspace_name}-lt-"
 
   block_device_mappings {
     device_name = "/dev/xvda"
@@ -47,7 +48,8 @@ resource "aws_launch_template" "nodegroup_launch_template" {
 resource "aws_eks_node_group" "workspace_node_group" {
   cluster_name    = data.aws_eks_cluster.current.name
   version         = var.kube_version
-  node_group_name = "${local.e6data_workspace_name}-${element(split(".", var.kube_version),1)}"
+  # node_group_name = "${local.e6data_workspace_name}-${element(split(".", var.kube_version),1)}"
+  node_group_name_prefix = "${local.e6data_workspace_name}-ng-"
   node_role_arn   = aws_iam_role.eks_nodegroup_iam_role.arn
   ami_type        = "AL2_ARM_64"
   # subnet_ids      = data.aws_eks_cluster.current.vpc_config[0].subnet_ids
