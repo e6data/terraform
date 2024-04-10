9de85d038da39012a18ef9323d24458b85185142
Harshith <harshith@e6x.io>
2024-04-10 22:06:08 +0530

    <harshith>limit permissions for cross account role

--- e6data_with_new_eks/provider.tf
+++ e6data_with_new_eks/provider.tf
@@ -1,24 +1,24 @@
-# # Configure the AWS Provider
-# provider "aws" {
-#   region  = var.aws_region
-#   # access_key = "<Access key ID>"
-#   # secret_key = "<Secret access key>"
-#   default_tags {
-#     tags = var.cost_tags
-#   }
-# }
+# Configure the AWS Provider
+provider "aws" {
+  region  = var.aws_region
+  # access_key = "<Access key ID>"
+  # secret_key = "<Secret access key>"
+  default_tags {
+    tags = var.cost_tags
+  }
+}
 
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key"
-#     region = "us-east-1"
-#   }
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
   
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.35.0"
-#     }
-#   } 
-# }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  } 
+}
