f52864829053fc857507de34d531b09b113ffde2
Harshith <harshith@e6x.io>
2024-05-15 10:35:36 +0530

    <harshith>removed duplicate node role in the eks module

--- e6data_with_new_eks/modules/eks/cluster_role_policy.tf
+++ e6data_with_new_eks/modules/eks/cluster_role_policy.tf
@@ -14,13 +14,3 @@ resource "aws_iam_role" "iam_eks_cluster_role" {
   managed_policy_arns = var.iam_eks_cluster_policy_arn
   assume_role_policy = data.aws_iam_policy_document.iam_eks_cluster_assume_policy.json
 }
-
-data "aws_iam_policy_document" "iam_eks_node_assume_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
