7a6340ab4cafb4aeeef5998394f2d85085464976
Harshith <harshith@e6x.io>
2024-05-31 16:06:49 +0530

    <harshith>changes for existing eks cluster

--- aws/e6data_with_existing_eks/provider.tf
+++ aws/e6data_with_existing_eks/provider.tf
@@ -1,36 +1,29 @@
-# Comment out this block if storing Terraform state is required.
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.22.0"
-    }
+# Configure the AWS Provider
+provider "aws" {
+  region  = var.aws_region
+  # access_key = "<Access key ID>"
+  # secret_key = "<Secret access key>"
+  default_tags {
+    tags = var.cost_tags
   }
 }
 
-# Uncomment this block if storing Terraform state is required & update the bucket name.
-
-# terraform {
-#  backend "s3" {
-#    bucket = "<bucket_name_to_store_the_tfstate_file>"
-#    key    = "terraform/state.tfstate"
-#    region = var.aws_region
-#  }
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "4.22.0"
-#    }
-#  }
-# }
-
-provider "aws" {
-  region = var.aws_region
-#  access_key = "YOUR_AWS_ACCESS_KEY"
-#  secret_key = "YOUR_AWS_SECRET_KEY"
-  default_tags {
-    tags = {
-      app  = "e6data"
-    }
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
   }
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.35.0"
+    }
+
+    kubectl = {
+      source = "alekc/kubectl"
+      version = "2.0.4"
+    }
+  } 
 }
