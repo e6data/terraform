f5dddaa728a17cdcc6ee0fe6ee917e35f21b6227
pranav-e6x <pranav@e6x.io>
2024-06-05 12:46:35 +0530

    <pranav>canonical format outputs.tf

--- aws/e6data_with_new_eks/modules/network/outputs.tf
+++ aws/e6data_with_new_eks/modules/network/outputs.tf
@@ -1,11 +1,11 @@
 output "public_subnet_ids" {
   description = "IDs of the created public subnets"
-  value = [for subnet in aws_subnet.public : subnet.id]
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
 
 output "private_subnet_ids" {
   description = "IDs of the created private subnets"
-  value = [for subnet in aws_subnet.private : subnet.id]
+  value       = [for subnet in aws_subnet.private : subnet.id]
 }
 
 output "public_route_table_id" {
@@ -20,15 +20,15 @@ output "private_route_table_id" {
 
 output "subnet_ids" {
   description = "IDs of all subnets"
-  value = concat([for subnet in aws_subnet.private : subnet.id] , [for subnet in aws_subnet.public : subnet.id])
+  value       = concat([for subnet in aws_subnet.private : subnet.id], [for subnet in aws_subnet.public : subnet.id])
 }
 
 output "vpc_id" {
   description = "The vpc ID"
-  value = aws_vpc.vpc.id
+  value       = aws_vpc.vpc.id
 }
 
 output "vpc_cidr" {
   description = "The vpc CIDR"
-  value = aws_vpc.vpc.cidr_block
+  value       = aws_vpc.vpc.cidr_block
 }
