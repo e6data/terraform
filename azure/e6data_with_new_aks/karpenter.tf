aa89472b8cfb938a148394d9c810bd4a4826106b
hemant-uniphi <hemant@e6x.io>
2024-07-02 14:20:36 +0530

    adding manifest and updating az var

--- azure/e6data_with_new_aks/karpenter.tf
+++ azure/e6data_with_new_aks/karpenter.tf
@@ -35,3 +35,39 @@ module "karpenter" {
   
   depends_on = [module.network]
 }
+
+
+data "azurerm_availability_zones" "available" {
+  location = var.region
+}
+# Data source to fetch and template Karpenter provisioner manifests
+data "kubectl_path_documents" "provisioner_manifests" {
+  pattern = "./karpenter-provisioner-manifests/*.yaml"
+  vars = {
+    workspace_name           = var.workspace_name
+    available_zones          = jsonencode(data.azurerm_availability_zones.available.zones)
+    cluster_name             = module.eks.cluster_name
+    instance_family          = jsonencode(var.nodepool_instance_family)
+    karpenter_node_role_name = aws_iam_role.karpenter_node_role.name
+    volume_size              = var.eks_disk_size
+    nodeclass_name           = local.e6data_nodeclass_name
+    nodepool_name            = local.e6data_nodepool_name
+    tags = jsonencode(
+      merge(
+        var.cost_tags,
+        {
+          "Name" = local.e6data_workspace_name
+        }
+      )
+    )
+    nodepool_cpu_limits = var.nodepool_cpu_limits
+  }
+  depends_on = [data.aws_availability_zones.available, aws_iam_role.karpenter_node_role, module.eks]
+}
+
+resource "kubectl_manifest" "provisioners" {
+  count     = 2
+  yaml_body = values(data.kubectl_path_documents.provisioner_manifests.manifests)[count.index]
+
+  depends_on = [module.karpeneter_deployment]
+}
