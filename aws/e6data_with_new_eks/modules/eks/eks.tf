f39160ef76581d4ff5383f577418262e594627e1
pranav-e6x <pranav@e6x.io>
2024-07-03 14:41:28 +0530

    feat: added cloudwatch log retention resource

--- aws/e6data_with_new_eks/modules/eks/eks.tf
+++ aws/e6data_with_new_eks/modules/eks/eks.tf
@@ -9,6 +9,7 @@ terraform {
 
 # Create the EKS cluster
 resource "aws_eks_cluster" "eks" {
+  depends_on                = [aws_cloudwatch_log_group.eks]
   name                      = "e6data-${var.cluster_name}"
   role_arn                  = aws_iam_role.iam_eks_cluster_role.arn
   version                   = var.kube_version
@@ -26,6 +27,5 @@ resource "aws_cloudwatch_log_group" "eks" {
   name              = "/aws/eks/${aws_eks_cluster.eks.name}/cluster"
   retention_in_days = var.cloudwatch_log_retention_in_days
 
-  tags       = var.cost_tags
-  depends_on = [aws_eks_cluster.eks]
+  tags = var.cost_tags
 }
