d7b70e1e61d43bf07d5d9e6d9d3988b67056593c
pranav-e6x <pranav@e6x.io>
2024-07-19 16:49:33 +0530

    feat: helm chart version update

--- aws/e6data_with_existing_vpc/modules/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  type        = string
-  description = "Name of S3 bucket"
-}
-
-variable "cost_tags" {
-  type        = map(string)
-  description = "Tags for isolation and cost management"
-}
--- gcp/e6data_with_new_gke/modules/gke_cluster/gke.tf
+++ gcp/e6data_with_new_gke/modules/gke_cluster/gke.tf
@@ -29,7 +29,7 @@ resource "google_container_cluster" "gke_cluster" {
   private_cluster_config {
     enable_private_nodes    = true
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
-    enable_private_endpoint = false
+    enable_private_endpoint = true
   }
 
   ip_allocation_policy {
