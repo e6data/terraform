cb41ae678669c4018c8324a538f0756ba222e6c3
Harshith <104987110+Harshithraj24@users.noreply.github.com>
2024-08-16 15:51:05 +0530

    <harshith>updated the helm chart repo (#121)

--- aws/e6data_with_new_eks/helm.tf
+++ aws/e6data_with_new_eks/helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "e6data_workspace_deployment" {
   provider = helm.e6data
 
   name             = var.workspace_name
-  repository       = "https://e6x-labs.github.io/helm-charts/"
+  repository       = "https://e6data.github.io/helm-charts/"
   chart            = "workspace"
   namespace        = var.kubernetes_namespace
   create_namespace = true
