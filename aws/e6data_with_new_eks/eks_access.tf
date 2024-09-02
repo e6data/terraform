f92c0b2e257c25cd2f814dc71a485e941e7afedd
pranav-e6x <pranav@e6x.io>
2024-09-02 16:16:38 +0530

    feat: updated eks auth type to access entry

---
+++ aws/e6data_with_new_eks/eks_access.tf
@@ -0,0 +1,39 @@
+resource "aws_eks_access_entry" "aws_auth" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.e6data_cross_account_role.arn
+  type          = "STANDARD"
+  user_name     = "e6data-${var.workspace_name}-user"
+  depends_on    = [module.eks]
+}
+
+resource "aws_eks_access_policy_association" "aws_auth_policy" {
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.e6data_cross_account_role.arn
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.aws_auth, module.eks]
+}
+
+resource "aws_eks_access_entry" "aws_karpenter_auth" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.karpenter_node_role.arn
+  type          = "STANDARD"
+  user_name     = "e6dsata-${var.workspace_name}-user"
+  depends_on    = [module.eks]
+}
+
+resource "aws_eks_access_policy_association" "aws_karpenter_auth_policy" {
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.karpenter_node_role.arn
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.aws_karpenter_auth, module.eks]
+}
--- aws/e6data_with_new_eks/helm.tf
+++ aws/e6data_with_new_eks/helm.tf
@@ -17,19 +17,3 @@ resource "helm_release" "e6data_workspace_deployment" {
 
   depends_on = [module.eks, aws_eks_node_group.default_node_group]
 }
-
-resource "aws_eks_access_entry" "aws_auth_update" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = aws_iam_role.e6data_cross_account_role.arn
-  type          = "STANDARD"
-  user_name     = "e6data-${var.workspace_name}-user"
-  depends_on    = [module.eks]
-}
-
-resource "aws_eks_access_entry" "aws_karpenter_auth_update" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = aws_iam_role.karpenter_node_role.arn
-  type          = "STANDARD"
-  user_name     = "e6dsata-${var.workspace_name}-user"
-  depends_on    = [module.eks]
-}
