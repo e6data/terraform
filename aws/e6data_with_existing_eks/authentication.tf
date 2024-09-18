1729b0e75b0206573d088b8721b9eb7110a3e011
pranav-e6x <pranav@e6x.io>
2024-09-18 20:40:58 +0530

    fix: added access entry to existing eks

--- aws/e6data_with_existing_eks/authentication.tf
+++ aws/e6data_with_existing_eks/authentication.tf
@@ -1,5 +1,5 @@
 locals {
-  authentication_mode = "CONFIG_MAP" // or "API_AND_CONFIG_MAP"
+  authentication_mode = data.aws_eks_cluster.current.authentication_mode
 }
 
 module "e6data_configmap" {
@@ -9,8 +9,7 @@ module "e6data_configmap" {
   karpenter_node_role_arn = aws_iam_role.karpenter_node_role.arn
   cross_account_role_arn  = aws_iam_role.e6data_cross_account_role.arn
   aws_command_line_path   = var.aws_command_line_path
-
-  count = local.authentication_mode == "CONFIG_MAP" ? 1 : 0
+  count                   = local.authentication_mode == "CONFIG_MAP" ? 1 : 0
 }
 
 module "e6data_configmap_and_api" {
@@ -18,6 +17,5 @@ module "e6data_configmap_and_api" {
   workspace_name         = var.workspace_name
   eks_cluster_name       = var.eks_cluster_name
   cross_account_role_arn = aws_iam_role.e6data_cross_account_role.arn
-
-  count = local.authentication_mode == "API_AND_CONFIG_MAP" ? 1 : 0
+  count                  = local.authentication_mode == "API_AND_CONFIG_MAP" ? 1 : 0
 }
