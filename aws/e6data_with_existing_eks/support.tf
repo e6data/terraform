831a1916b401b625beb878d5ac90b51f3eee15e9
pranav-e6x <pranav@e6x.io>
2024-09-18 20:46:18 +0530

    fix: added access entry to existing eks

--- aws/e6data_with_existing_eks/support.tf
+++ aws/e6data_with_existing_eks/support.tf
@@ -1,6 +1,5 @@
 locals {
   short_workspace_name        = substr(var.workspace_name, 0, 5)
-  authentication_mode         = data.aws_eks_cluster.current.authentication_mode
   e6data_workspace_name       = "e6data-workspace-${local.short_workspace_name}"
   bucket_names_with_full_path = [for bucket_name in var.bucket_names : "arn:aws:s3:::${bucket_name}/*"]
   bucket_names_with_arn       = [for bucket_name in var.bucket_names : "arn:aws:s3:::${bucket_name}"]
