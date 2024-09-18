76b5a34f319d4988ddcdd5ba109c95762c97d3e7
pranav-e6x <pranav@e6x.io>
2024-09-18 15:16:34 +0530

    fix: added access entry to existing eks

--- aws/e6data_with_existing_eks/eks_access.tf
+++ aws/e6data_with_existing_eks/eks_access.tf
@@ -1,19 +1,19 @@
 resource "aws_eks_access_entry" "aws_auth" {
-  cluster_name  = data.aws_eks_cluster.current.cluster_name
+  cluster_name  = data.aws_eks_cluster.current.name
   principal_arn = aws_iam_role.e6data_cross_account_role.arn
   type          = "STANDARD"
   user_name     = "e6data-${var.workspace_name}-user"
 }
 
 resource "aws_eks_access_entry" "tf_runner" {
-  cluster_name  = data.aws_eks_cluster.current.cluster_name
+  cluster_name  = data.aws_eks_cluster.current.name
   principal_arn = local.role_arn
   type          = "STANDARD"
   user_name     = "terraform-user"
 }
 
 resource "aws_eks_access_policy_association" "tf_runner_auth_policy" {
-  cluster_name  = data.aws_eks_cluster.current.cluster_name
+  cluster_name  = data.aws_eks_cluster.current.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = local.role_arn
 
