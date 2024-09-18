806679bb032abc2e58f46dd937b09eb07942c421
pranav-e6x <pranav@e6x.io>
2024-09-18 22:40:16 +0530

    fix: added access entry to existing eks

--- aws/e6data_with_existing_eks/authentication.tf
+++ aws/e6data_with_existing_eks/authentication.tf
@@ -1,9 +1,10 @@
+
 module "e6data_configmap_and_api" {
   source                 = "./modules/configmap_and_api"
   count = data.aws_eks_cluster.current.access_config[0].authentication_mode == "API_AND_CONFIG_MAP" ? 1 : 0
 
-  eks_cluster_name = var.eks_cluster_name
-  cross_account_role_arn = var.cross_account_role_arn
-  principal_arn = var.principal_arn
-  workspace_name = var.workspace_name
+  workspace_name         = var.workspace_name
+  eks_cluster_name       = var.eks_cluster_name
+  principal_arn          = local.role_arn
+  cross_account_role_arn = aws_iam_role.e6data_cross_account_role.arn
 }
