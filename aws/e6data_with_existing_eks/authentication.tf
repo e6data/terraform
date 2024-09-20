fd7b2b544c67f6f545d73598d5a45bd065d99956
pranav-e6x <pranav@e6x.io>
2024-09-20 18:21:29 +0530

    feat: added existing eks API conditioning

--- aws/e6data_with_existing_eks/authentication.tf
+++ aws/e6data_with_existing_eks/authentication.tf
@@ -1,6 +1,6 @@
 module "e6data_configmap_and_api" {
   source                 = "./modules/configmap_and_api"
-  count = data.aws_eks_cluster.current.access_config[0].authentication_mode == "API_AND_CONFIG_MAP" ? 1 : 0
+  count = contains(["API", "API_AND_CONFIG_MAP"], data.aws_eks_cluster.current.access_config[0].authentication_mode) ? 1 : 0
 
   workspace_name         = var.workspace_name
   eks_cluster_name       = var.eks_cluster_name
