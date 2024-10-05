afd93fcd7b470169bbec77c09d4973e4329e2306
pranav-e6x <pranav@e6x.io>
2024-10-05 12:31:15 +0530

    feat: added support for gke autopilot

--- gcp/e6data_with_existing_autopilot_gke/main.tf
+++ gcp/e6data_with_existing_autopilot_gke/main.tf
@@ -100,6 +100,7 @@ resource "google_project_iam_custom_role" "e6dataclusterViewer" {
   title       = "e6data ${var.workspace_name} clusterViewer ${random_string.random.result}"
   description = "kubernetes container clusterViewer access"
   permissions = [
+    "container.secrets.create",
     "container.clusters.get",
     "container.clusters.list",
     "container.roleBindings.get",
