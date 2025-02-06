b0613b8a4c8e170dfe96fe08f42160ffdd7a3eae
Srinath Prabhu <srinath@e6data.com>
2025-02-06 19:34:56 +0530

    fixes

--- aws/e6data_with_new_eks/storage.tf
+++ aws/e6data_with_new_eks/storage.tf
@@ -1,8 +1,9 @@
 resource "aws_eks_addon" "ebs_storage_driver" {
   cluster_name = module.eks.cluster_name
   addon_name   = "aws-ebs-csi-driver"
+  service_account_role_arn = module.ebs_driver_oidc.oidc_role_arn
 
-  depends_on = [  ]
+  depends_on = [ module.ebs_driver_oidc ]
 
 
 }
