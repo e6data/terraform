46970cf5b45eb4ae7ced11cd8eb7d252a6c94b37
pranav-e6x <pranav@e6x.io>
2025-02-19 15:49:58 +0530

    feat: added 2nd NAT gateway

--- aws/e6data_with_existing_vpc/modules/network/nat.tf
+++ aws/e6data_with_existing_vpc/modules/network/nat.tf
@@ -1,13 +1,27 @@
-resource "aws_eip" "nat" {
+resource "aws_eip" "nat_1" {
   tags = {
-    Name = "${var.env}-${var.workspace_name}-nat-EIP"
+    Name = "${var.env}-${var.workspace_name}-nat-1-EIP"
   }
 }
 
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat.id
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_1.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "${var.env}-${var.workspace_name}-nat-1-GW"
+  }
+}
+
+resource "aws_eip" "nat_2" {
+  tags = {
+    Name = "${var.env}-${var.workspace_name}-nat-2-EIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_2.id
   subnet_id     = aws_subnet.public[1].id
   tags = {
-    Name = "${var.env}-${var.workspace_name}-nat-GW"
+    Name = "${var.env}-${var.workspace_name}-nat-2-GW"
   }
 }
