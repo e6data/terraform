b14315fe780a62c884e4403464dd5c48f30916e0
Harshith <harshith@e6x.io>
2025-04-24 23:59:00 +0530

    feat: cross account modules

---
+++ aws/e6data_with_existing_eks/modules/cross_acc_querying_compute_plane/main.tf
@@ -0,0 +1,54 @@
+locals {
+  policy_name = "${var.identifier}-${var.query_type}-query-policy" 
+}
+
+resource "aws_iam_policy" "query_policy" {
+  name        = local.policy_name
+  description = "IAM policy for ${var.query_type} query"
+  policy      = data.aws_iam_policy_document.cross_acc_query.json
+}
+
+data "aws_iam_policy_document" "cross_acc_query" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    resources = [
+      var.cross_acc_query_role_arn
+    ]
+
+    dynamic "condition" {
+      for_each = var.external_id != "" ? [1] : []
+      content {
+        test     = "StringEquals"
+        variable = "sts:ExternalId"
+        values   = [var.external_id]
+      }
+    }
+  }
+
+  statement {
+    actions = ["sts:TagSession"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "glue:GetDatabase*",
+      "glue:GetTable*",
+      "glue:GetPartitions"
+    ]
+    resources = [
+      "arn:aws:glue:${var.region}:${var.cross_account_id}:catalog",
+      "arn:aws:glue:${var.region}:${var.cross_account_id}:database/*",
+      "arn:aws:glue:${var.region}:${var.cross_account_id}:table/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "query_policy_attachment" {
+  policy_arn = aws_iam_policy.query_policy.arn
+  role       = var.engine_role_name
+}
