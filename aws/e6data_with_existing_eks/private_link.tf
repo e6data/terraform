1b9d7dd021a1a2c4a2cf37864f1534fa6834d5bf
Harshith <harshith@e6x.io>
2025-06-18 21:04:13 +0530

    feat: private_link and karpenter in existing eks

--- aws/e6data_with_existing_eks/private_link.tf
+++ aws/e6data_with_existing_eks/private_link.tf
@@ -19,7 +19,7 @@ module "compute_plane_endpoint_services" {
 }
 
 module "compute_plane_vpc_endpoints" {
-  source = "../modules/interface_vpc_endpoints"
+  source = "./modules/interface_vpc_endpoints"
 
   for_each = var.interface_vpc_endpoints
 
@@ -34,7 +34,7 @@ module "compute_plane_vpc_endpoints" {
 }
 
 module "compute_plane_gateway_vpc_endpoints" {
-  source = "../modules/gateway_vpc_endpoints"
+  source = "./modules/gateway_vpc_endpoints"
 
   for_each = var.gateway_vpc_endpoints
 
