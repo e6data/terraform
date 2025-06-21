3e75d14c96751bf6a1e156e7e4343afeb2862c96
Harshith <harshith@e6x.io>
2025-06-21 21:10:25 +0530

    karpenter version mismatch fix

--- aws/e6data_with_existing_vpc/modules/eks/eks.tf
+++ aws/e6data_with_existing_vpc/modules/eks/eks.tf
@@ -18,6 +18,7 @@ resource "aws_eks_cluster" "eks" {
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
     security_group_ids      = var.security_group_ids
   }
 
