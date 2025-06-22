0e1c0ebf2e838bdbb1328bcf9fb359c99d5b20c8
Harshith <harshith@e6x.io>
2025-06-22 20:40:20 +0530

    karpenter version mismatch fix

--- aws/e6data_with_existing_vpc/modules/eks/oidc.tf
+++ aws/e6data_with_existing_vpc/modules/eks/oidc.tf
@@ -1,10 +1,10 @@
-data "tls_certificate" "oidc_tls" {
-  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
-  verify_chain  = false
-}
+# data "tls_certificate" "oidc_tls" {
+#   url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+#   verify_chain  = false
+# }
 
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_tls.certificates[0].sha1_fingerprint]
+  thumbprint_list = ["test"]
   url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
 }
