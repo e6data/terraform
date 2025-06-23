b6322dc5314d3321a578d98a962150643843cc1d
Harshith <harshith@e6x.io>
2025-06-23 21:40:39 +0530

    mondaynight

--- aws/e6data_with_existing_vpc/msk_connection.tf
+++ aws/e6data_with_existing_vpc/msk_connection.tf
@@ -3,5 +3,29 @@ resource "aws_msk_vpc_connection" "msk_connect" {
   target_cluster_arn = var.msk_cluster_arn
   vpc_id             = module.network.vpc_id
   client_subnets     = module.network.private_subnet_ids
-  security_groups    = [module.security_group.security_group_id]
+  security_groups    = [aws_security_group.msk_vpc_connection.id]
+}
+
+resource "aws_security_group" "msk_vpc_connection" {
+  name        = "msk-vpc-connection-sg"
+  description = "Allow ports 14098-14100 for MSK VPC connection"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    from_port   = 14098
+    to_port     = 14100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "msk-vpc-connection-sg"
+  }
 }
