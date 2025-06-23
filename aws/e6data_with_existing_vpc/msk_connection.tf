d8e972f6923331d44e21ef0c835c0b0cd2f079d4
Harshith <harshith@e6x.io>
2025-06-23 15:09:51 +0530

    karpenter version mismatch fix

--- aws/e6data_with_existing_vpc/msk_connection.tf
+++ aws/e6data_with_existing_vpc/msk_connection.tf
@@ -2,6 +2,6 @@ resource "aws_msk_vpc_connection" "test" {
   authentication     = "SASL_IAM"
   target_cluster_arn = var.msk_cluster_arn
   vpc_id             = module.network.vpc_id
-  client_subnets     = module.network.private_subnets
+  client_subnets     = module.network.private_subnet_ids
   security_groups    = [module.security_group.security_group_id]
 }
