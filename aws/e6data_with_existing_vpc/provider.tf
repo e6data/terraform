e4c689c21bab669c829fc11f70491ec0ebf6345f
Harshith <harshith@e6x.io>
2025-06-23 08:31:38 +0530

    karpenter version mismatch fix

--- aws/e6data_with_existing_vpc/provider.tf
+++ aws/e6data_with_existing_vpc/provider.tf
@@ -37,8 +37,46 @@ terraform {
     }
   }
 }
-provider "tls" {
-  proxy {
-    from_env = false
+
+
+
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_eks_cluster" "e6data" {
+  name = "e6data-nonatcpp"
+}
+
+data "aws_eks_cluster_auth" "e6data" {
+  name = data.aws_eks_cluster.e6data.name
+}
+
+provider "kubernetes" {
+  alias                   = "e6data"
+  host                    = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+  token                   = data.aws_eks_cluster_auth.e6data.token
+  cluster_ca_certificate  = null
+  insecure                = true
+}
+
+provider "kubectl" {
+  host                   = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+  token                  = data.aws_eks_cluster_auth.e6data.token
+  cluster_ca_certificate = null
+  load_config_file	 = false
+  insecure                = true
+}
+
+provider "helm" {
+  alias = "e6data"
+  kubernetes {
+    host                   = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+    token                  = data.aws_eks_cluster_auth.e6data.token
+    cluster_ca_certificate = null
+    insecure               = true
   }
 }
