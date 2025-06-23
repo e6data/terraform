5b77c06562fc512865dac4afbe96956d0b97bce3
Harshith <harshith@e6x.io>
2025-06-23 23:58:34 +0530

    eks internal

--- aws/e6data_with_existing_vpc/modules/eks_access_internal/outputs.tf
+++ aws/e6data_with_existing_vpc/modules/eks_access_internal/outputs.tf
@@ -1,3 +1,7 @@
 output "endpoint_service_name" {
-  value = one(module.eks_private_link.*.endpoint_service_name)
+  value = aws_vpc_endpoint_service.eks_endpoint_service.service_name
+}
+
+output "vpc_endpoint_service_id" {
+  value = aws_vpc_endpoint_service.eks_endpoint_service.service_name
 }
