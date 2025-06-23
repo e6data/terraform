ada27fd4b5481a5fc8059e50b33f2b929f23a5d0
Harshith <harshith@e6x.io>
2025-06-23 11:05:37 +0530

    karpenter version mismatch fix

--- aws/e6data_with_existing_vpc/eks.tf
+++ aws/e6data_with_existing_vpc/eks.tf
@@ -46,14 +46,14 @@ data "aws_eks_cluster_auth" "target_eks_auth" {
 
 provider "kubernetes" {
   alias                   = "e6data"
-  host                    = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+  host                    = "https://vpce-05a70c3362ee7a24a-wa4yyf16.vpce-svc-058b78c65cd75fa2e.us-east-1.vpce.amazonaws.com"
   token                  = data.aws_eks_cluster_auth.target_eks_auth.token
   cluster_ca_certificate  = null
   insecure                = true
 }
 
 provider "kubectl" {
-  host                   = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+  host                   = "https://vpce-05a70c3362ee7a24a-wa4yyf16.vpce-svc-058b78c65cd75fa2e.us-east-1.vpce.amazonaws.com"
   token                  = data.aws_eks_cluster_auth.target_eks_auth.token
   cluster_ca_certificate = null
   load_config_file	 = false
@@ -63,7 +63,7 @@ provider "kubectl" {
 provider "helm" {
   alias = "e6data"
   kubernetes {
-    host                   = "https://vpce-00ed8a0bd90bd659f-rodosog9.vpce-svc-00a0b40ce97c9fa1a.us-east-1.vpce.amazonaws.com"
+    host                   = "https://vpce-05a70c3362ee7a24a-wa4yyf16.vpce-svc-058b78c65cd75fa2e.us-east-1.vpce.amazonaws.com"
   token                  = data.aws_eks_cluster_auth.target_eks_auth.token
     cluster_ca_certificate = null
     insecure               = true
