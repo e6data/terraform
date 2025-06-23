2c38363c0403fd291d18ff596d6159f2dc998431
Harshith <harshith@e6x.io>
2025-06-24 00:05:53 +0530

    eks internal

--- aws/e6data_with_existing_vpc/modules/eks_access_internal/nlb.tf
+++ aws/e6data_with_existing_vpc/modules/eks_access_internal/nlb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "eks_nlb" {
-  name               = "eks-int-nlb-${var.eks_cluster_name}"
+  name               = "int-nlb-${var.eks_cluster_name}"
   internal           = true
   load_balancer_type = "network"
   subnets            = var.subnet_ids
