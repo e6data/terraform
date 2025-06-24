e4a2f0fb871559f9377cda3c22d4eaf6ba2372a4
Harshith <harshith@e6x.io>
2025-06-24 19:20:15 +0530

    eks internal

--- aws/e6data_with_existing_vpc/eks_endpoint.tf
+++ aws/e6data_with_existing_vpc/eks_endpoint.tf
@@ -3,11 +3,14 @@ data "aws_network_interfaces" "eks_nic" {
     name   = "description"
     values = ["Amazon EKS ${module.eks.cluster_name}"]
   }
+  depends_on = [ module.eks ]
 }
 
 data "aws_network_interface" "eks_nic" {
   for_each = toset(data.aws_network_interfaces.eks_nic.ids)
   id       = each.key
+
+    depends_on = [ module.eks,aws_network_interfaces.eks_nic ]
 }
 
 resource "aws_vpc_endpoint_connection_accepter" "accept" {
