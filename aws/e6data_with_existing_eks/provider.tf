dcafb845908a15a059b84253a0e48cc9488afa55
Siddhanth03 <siddhanth@e6x.io>
2025-06-25 13:15:53 +0530

    Update provider.tf

--- aws/e6data_with_existing_eks/provider.tf
+++ aws/e6data_with_existing_eks/provider.tf
@@ -25,5 +25,14 @@ terraform {
       source  = "alekc/kubectl"
       version = "2.0.4"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
