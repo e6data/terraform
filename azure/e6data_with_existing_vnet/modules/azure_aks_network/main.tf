09ea163bbe862988c842d74779bfad6a35201762
Harshith <harshith@e6x.io>
2025-09-17 17:46:14 +0530

    agic in exisiting vnet

--- azure/e6data_with_existing_vnet/modules/azure_aks_network/main.tf
+++ azure/e6data_with_existing_vnet/modules/azure_aks_network/main.tf
@@ -28,3 +28,43 @@ resource "azurerm_subnet" "aci" {
     }
   }
 }
+
+# Create Application Gateway for Containers (ALB) subnet
+resource "azurerm_subnet" "alb" {
+  count = var.create_alb_subnet ? 1 : 0
+  
+  name                 = format("%s-subnet-%s", "${var.prefix}", "alb")
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [cidrsubnet(tolist(azurerm_virtual_network.vnet.address_space)[0], 8, 80)]
+
+  # Delegate subnet to be used by Application Gateway for Containers
+  delegation {
+    name = "alb-delegation"
+
+    service_delegation {
+      name    = "Microsoft.ServiceNetworking/trafficControllers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+# Create private subnet for internal Application Gateway for Containers
+resource "azurerm_subnet" "alb_internal" {
+  count = var.create_alb_internal_subnet ? 1 : 0
+  
+  name                 = format("%s-subnet-%s", "${var.prefix}", "alb-internal")
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [cidrsubnet(tolist(azurerm_virtual_network.vnet.address_space)[0], 8, 81)]
+
+  # Delegate subnet to be used by Application Gateway for Containers (internal)
+  delegation {
+    name = "alb-internal-delegation"
+
+    service_delegation {
+      name    = "Microsoft.ServiceNetworking/trafficControllers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
